213347f178edc612e477edbfa60bb83310595ad6
ADXM TECH <147929248+AdamKhelladi@users.noreply.github.com>
2024-01-01 17:14:16 +0100

    Add files via upload
    This Python script utilizes the pytube library and tkinter for building a simple YouTube video downloader. Users are prompted to enter a YouTube URL and select a directory for saving the downloaded video. The script then fetches the highest resolution MP4 video stream available for download and saves it to the specified directory.
    
    Key Features:
    
    User-friendly with a graphical interface for selecting the download location.
    Utilizes the pytube library for handling YouTube video downloads.
    Allows users to input a YouTube URL and choose a local directory for saving the video.
    Usage:
    
    Run the script.
    Enter a valid YouTube URL when prompted.
    Use the file dialog to choose a directory for saving the video.
    The script fetches the highest resolution MP4 video stream and downloads it to the specified location.
    Note:
    Ensure that you have the required dependencies installed (pytube). The script showcases a basic example of a YouTube video downloader and can be extended or modified based on specific use cases or additional functionalities.

---
+++ YTDownloader.py
@@ -0,0 +1,45 @@
+# YouTube Video Downloader
+
+# Advanced Code
+
+from pytube import YouTube
+import tkinter as tk
+from tkinter import filedialog 
+import time
+
+def download_video(url, save_path) : 
+  try:
+    yt = YouTube(url)
+
+    streams = yt.streams.filter(progressive=True, file_extension="mp4")
+    heighest_res_stream = streams.get_by_resolution(480)
+
+    time.sleep(10)
+
+    heighest_res_stream.download(output_path= save_path)
+    print("Video Dowloaded Successfully!")
+
+  except Exception as e: 
+    print(e)
+
+def open_file_dialog(): 
+  folder = filedialog.askdirectory()
+  
+  if folder : 
+    print(f"Selected Folder: {folder}")
+  
+  return folder
+
+if __name__ == "__main__" : 
+  root = tk.Tk()
+  root.withdraw()
+
+  video_url = input("Please Enter A YouTubbe Url: ")
+  save_dir = open_file_dialog()
+
+  if save_dir:
+    print("Started Download...") 
+    download_video(video_url, save_dir)
+
+  else: 
+    print("Invalid Save Location.")
